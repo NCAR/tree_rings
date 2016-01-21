20549f8f88c85dc001cb83a55c5a3ea98f675697
Randy Russell <rrussell@ucar.edu>
2016-01-21 14:38:39 -0700

    Added multi-level game code to Preloader; added some relevant image files and sprite sheets

--- Preloader.js
+++ Preloader.js
@@ -9,7 +9,7 @@ TreeRings.Preloader.prototype = {
 	create: function() {
 		this.preloadBar.cropEnabled = false;
 		
-    	this.state.start('StartMenu');
+    	//this.state.start('StartMenu');
 	},
 	init: function() {
 		
@@ -31,8 +31,14 @@ TreeRings.Preloader.prototype = {
 		this.preloadBar = this.add.sprite(this.world.centerX, this.world.centerY, 'preloaderBar');
 		this.preloadBar.anchor.setTo(0.5, 0.5);
 		this.load.setPreloadSprite(this.preloadBar);
+		
 		this.titleText = this.add.image(this.world.centerX, this.world.centerY-100, 'titleimage');
 		this.titleText.anchor.setTo(0.5, 0.5);
+		
+		// For MultiLevel version
+		//this.load.json('dialog', 'assets/data/dialog.json');
+		// End new code for MultiLevel version
+		
 		this.load.image('titlescreen', 'assets/images/TitleBG.jpg');
 		this.load.bitmapFont('desyrel', 'assets/fonts/desyrel.png', 'assets/fonts/desyrel.xml');
 		this.load.image('game_bg', 'assets/images/game_bg_900x500.png');
@@ -53,6 +59,24 @@ TreeRings.Preloader.prototype = {
 		
         // 193x71 is the size of each frame.
         this.load.spritesheet('button_spritesheet', 'assets/images/spritesheets/button_sprite_sheet.png', 193, 71);
+		
+		
+		// For MultiLevel version
+		
+		//this.load.image('game_start_bkg', 'assets/images/game_start_bkg.png');
+		this.load.image('game_play_again_bkg', 'assets/images/game_play_again_bkg.png');
+		this.load.image('game_bg_level1', 'assets/images/game_bg_level1.png');
+		this.load.image('game_bg_level2', 'assets/images/game_bg_level2.png');
+		this.load.image('game_bg_level3', 'assets/images/game_bg_level3.png');
+		//this.load.image('game_bg', 'assets/images/game_bg_900x500.png');
+		
+        // 193x71 is the size of each frame
+        this.load.spritesheet('button_spritesheet_finish_level', 'assets/images/spritesheets/button_sprite_sheet_finish_level.png', 193, 71);
+        this.load.spritesheet('button_spritesheet_add_points', 'assets/images/spritesheets/button_sprite_sheet_add_points.png', 193, 71);
+        this.load.spritesheet('button_spritesheet_close_dialog', 'assets/images/spritesheets/button_sprite_sheet_close_dialog.png', 193, 71);
+		
+		console.log('Preloading complete');
+		// End new code for MultiLevel version
 	},
 	preRender: function() {
 		
