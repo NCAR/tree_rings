8189e98a510b82e388163d14d258e5c2d68d8453
Randy Russell <rrussell@ucar.edu>
2016-05-20 16:28:53 -0600

    Started adding shared code version of dialog boxes (Credits first) to SandBox level

--- javascript/SandBox.js
+++ javascript/SandBox.js
@@ -1,5 +1,7 @@
 TreeRings.SandBox = function(game) {
 	this.game = game; // keep reference to main game object
+	
+	this._creditsBtn;
 };
 
 TreeRings.SandBox.prototype.create = function() {
@@ -17,4 +19,24 @@ TreeRings.SandBox.prototype.buildLevel = function() {
 	circle1.lineStyle(2, 0x663300, 1);
 	circle1.beginFill(0xcc9900);
 	circle1.drawCircle(600, 50, 60);
+	
+	// Credits button and dialog box
+	this._initCredits();
+};
+
+/////////////
+// Credits //
+/////////////
+
+TreeRings.SandBox.prototype._initCredits = function() {
+	var xLoc = 10;
+	var yLoc = 200;
+	//var xLoc = this._helpBtn.x + this._helpBtn.width + 10;
+	this._creditsBtn = this.add.button(xLoc, yLoc, 'credits_btn_spritesheet', this._showCredits, this, 2, 1, 0);
+	this._creditsBtn.name = 'creditsBtn';
+};
+
+TreeRings.SandBox.prototype._showCredits = function(pointer) {
+    //this._drawDialog('credits');
+	console.log("Credits");
 };
